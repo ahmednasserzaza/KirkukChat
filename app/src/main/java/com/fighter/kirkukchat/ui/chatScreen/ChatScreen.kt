8409adda76e408bef55246b5aad4bbade4ed14f2
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-09-17 22:43:03 +0300

    create structure of ChatScreen

--- app/src/main/java/com/fighter/kirkukchat/ui/chatScreen/ChatScreen.kt
+++ app/src/main/java/com/fighter/kirkukchat/ui/chatScreen/ChatScreen.kt
@@ -1,2 +1,20 @@
 package com.fighter.kirkukchat.ui.chatScreen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ChatScreen(){
+
+}
+
+@Composable
+fun ChatScreenContent(){
+
+}
+
+@Preview
+@Composable
+fun PreviewChatScreen() {
+
+}
