c556a3c1295a7f3cbfd0b1db7751f6513eff9c6a
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-09-18 07:56:18 +0300

    provide local source for strings and images

---
+++ app/src/main/java/com/fighter/kirkukchat/di/ResourceProvider.kt
@@ -0,0 +1,21 @@
+package com.fighter.kirkukchat.di
+
+import android.content.Context
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ResourceProvider @Inject constructor(
+    @ApplicationContext private val context: Context
+) {
+    fun getString(@StringRes value: Int): String {
+        return context.getString(value)
+    }
+
+    fun getDrawable(@DrawableRes value: Int): Int {
+        return value
+    }
+}
